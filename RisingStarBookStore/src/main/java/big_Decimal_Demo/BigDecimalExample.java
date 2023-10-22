c4f4fcb9078a394cc7fb656f6c62df7e295d47b1
richahoipen <minhyemen@gmail.com>
2023-10-22 14:05:56 +0700

    commit

---
+++ RisingStarBookStore/src/main/java/big_Decimal_Demo/BigDecimalExample.java
@@ -0,0 +1,16 @@
+package big_Decimal_Demo;
+
+import java.math.BigDecimal;
+
+public class BigDecimalExample {
+    public static void main(String[] args) {
+        // Khởi tạo và thực hiện các phép toán với BigDecimal
+        BigDecimal num1 = new BigDecimal("23829934827984698264396298368263876187628716873678623767823");
+        BigDecimal num2 = new BigDecimal("3.2");
+
+        System.out.println("Big decimal: "+num1.add(new BigDecimal("32324.323")));
+
+     
+    }
+}
+
